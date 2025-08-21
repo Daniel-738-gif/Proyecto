9ed6a06a87f6d72665e0b4b9ff4d54472ec9831b
Daniel-738-gif <danir_0@hotmail.com>
2025-08-21 01:38:05 -0500

    Imagen de fondo
    transparencia del fondo

--- 1_Portafolio/script.js
+++ 1_Portafolio/script.js
@@ -1,13 +1,19 @@
-const btn = document.getElementById('themeToggle');
+const btn = document.getElementById('modo-oscuro');
 const body = document.body;
 
 btn.addEventListener('click', () => {
   body.classList.toggle('dark');
 
-  // Update button text/icon
-  if (body.classList.contains('dark')) {
-    btn.textContent = '☀️ Modo claro';
-  } else {
-    btn.textContent = '🌙 Modo oscuro';
-  }
-});
+    // Update button text/icon
+      if (body.classList.contains('dark')) 
+        {
+        btn.textContent = '☀️ Modo claro';
+        }   
+          else 
+        {
+          btn.textContent = '🌙 Modo oscuro';
+        }
+  } 
+
+
+);
