d01446142e505a4fec3a56da099819dffadf5ea4
Daniel-738-gif <danir_0@hotmail.com>
2025-08-26 23:52:10 -0500

    test-formulario
    test

--- 1_Portafolio/script.js
+++ 1_Portafolio/script.js
@@ -18,3 +18,18 @@ btn.addEventListener('click', () => {
 
 
 );
+// formulario//
+function openModal() {
+  document.getElementById('contactModal').style.display = 'block';
+}
+
+function closeModal() {
+  document.getElementById('contactModal').style.display = 'none';
+}
+
+window.onclick = function(event) {
+  const modal = document.getElementById('contactModal');
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+}
