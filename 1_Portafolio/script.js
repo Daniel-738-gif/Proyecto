7dfc0f666e005f869cd48eb16c9de8e9bee86382
Daniel-738-gif <danir_0@hotmail.com>
2025-08-30 23:21:26 -0500

    orden
    orden

--- 1_Portafolio/script.js
+++ 1_Portafolio/script.js
@@ -1,11 +1,11 @@
 const btn = document.getElementById('modo-oscuro');
 const body = document.body;
-const a = document.a;
+
 
 btn.addEventListener('click', () => {
   body.classList.toggle('dark');
 
-    // Alctualiza texto del botn + icono
+    // Alctualiza texto del boton + icono
       if (body.classList.contains('dark')) 
         {
         btn.textContent = '☀️ Modo claro';
