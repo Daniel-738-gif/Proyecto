fc1c399b11088605029daad57ce0756fa4e25d2f
Daniel-738-gif <danir_0@hotmail.com>
2025-10-28 23:45:53 -0500

    test
    test

--- 4_test-js/js.js
+++ 4_test-js/js.js
@@ -2,12 +2,5 @@ function mostrarHora() {
 	const ahora = new Date();
     console.log("Son las", ahora.toLocaleTimeString());
 }
-
-setInterval(mostrarHora, 1000);
-mostrarHora(); // Llamada inicial para mostrar la hora inmediatamente
-
-// Código adicional para pruebas
-module.exports = { mostrarHora };
-
-
-
+ 
+document.getElementById("mensaje").textContent = "¡Hola desde JS!";
